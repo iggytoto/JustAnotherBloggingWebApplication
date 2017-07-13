13d403ad9c489b7c80d73e2170dd07d3eb482974
esemenov89 <gitSobolev89>
2017-07-13 09:41:48 +0300

    add pagesCount for Sergey

--- src/main/java/leblogger/controllers/rest/PostController.java
+++ src/main/java/leblogger/controllers/rest/PostController.java
@@ -4,9 +4,11 @@ import leblogger.model.Post;
 import leblogger.services.BlogService;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 /**
@@ -16,23 +18,53 @@ import java.util.ArrayList;
 @RequestMapping(value = "/")
 public class PostController {
 
-    BlogService blogService;
+    private Environment env;
 
+    private BlogService blogService;
 
-    ArrayList<Post> posts = null;
+    private ArrayList<Post> posts = null;
 
     @Autowired
     public void setBlogService(BlogService blogService) {
         this.blogService = blogService;
     }
 
+    @Autowired
+    public void setEnv(Environment env) {
+        this.env = env;
+    }
+
     @RequestMapping(method = RequestMethod.GET)
     public ModelAndView viewPosts(ModelAndView mav) {
-        posts = blogService.getAllPosts();
+        long count = blogService.getCount();
+        int pagesDivider = Integer.parseInt(env.getRequiredProperty("pagesDivider"));
+        try {
+            posts = blogService.getRange(1,pagesDivider);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(posts.get(1).getId());
+
         mav.addObject("posts", posts);
-        mav.addObject("pagesCount", blogService.getCount()/10);
+        mav.addObject("pagesCount", count/pagesDivider+(count%pagesDivider>0?1:0));
         mav.setViewName("home.jsp");
-        System.out.println("hello");
+        return mav;
+    }
+
+    @RequestMapping(value="/{numberPageParam}", method = RequestMethod.GET)
+    public ModelAndView viewNumberPage(ModelAndView mav,@PathVariable String numberPageParam) {
+        int numberPage = Integer.parseInt(numberPageParam);
+        long count = blogService.getCount();
+        int pagesDivider = Integer.parseInt(env.getRequiredProperty("pagesDivider"));
+        try {
+            posts = blogService.getRange((numberPage-1)*pagesDivider+1, numberPage*pagesDivider+1);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        mav.addObject("posts", posts);
+        mav.addObject("pagesCount", count/pagesDivider+(count%pagesDivider>0?1:0));
+        mav.setViewName("home.jsp");
         return mav;
     }
 
