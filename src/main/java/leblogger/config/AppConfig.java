2bba45bfca37bd2d772efe2bb7dd4aac9529ba2b
iggytoto <iggytoto@gmail.com>
2017-07-13 10:49:14 +0300

    test in memory db implemented

--- src/main/java/leblogger/config/AppConfig.java
+++ src/main/java/leblogger/config/AppConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * Created by Iggytoto on 11.07.2017.
@@ -17,4 +20,6 @@ public class AppConfig {
         return Logger.getLogger(AppConfig.class);
     }
 
+    //TODO make something adequate...
+    public final static boolean IS_DEBUG = true;
 }
