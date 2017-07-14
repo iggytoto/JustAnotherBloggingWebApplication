c822c89038e3dcdd759403ec779d1cd6779d9e4e
esemenov89 <gitSobolev89>
2017-07-14 14:10:30 +0300

    before fix mapping in PostController

--- src/main/java/leblogger/controllers/PostController.java
+++ src/main/java/leblogger/controllers/PostController.java
@@ -1,4 +1,4 @@
-package leblogger.controllers.rest;
+package leblogger.controllers;
 
 import leblogger.model.Post;
 import leblogger.services.BlogService;
@@ -33,67 +33,32 @@ public class PostController {
         this.env = env;
     }
 
-    // забрать 10 записей
-    @RequestMapping(method = RequestMethod.GET)
-    public ModelAndView viewPosts(ModelAndView mav) {
-
-        long count = blogService.getCount();
-        int pagesDivider = Integer.parseInt(env.getRequiredProperty("pagesDivider"));
-
-        posts = blogService.getRange(0, pagesDivider - 1);
-
-        mav.addObject("posts", posts);
-        mav.addObject("pagesCount", count / pagesDivider + (count % pagesDivider > 0 ? 1 : 0));
-        mav.setViewName("home.jsp");
-        return mav;
-    }
-
-    // перейти на страницу
-    @RequestMapping(value = "/{numberPageParam}", method = RequestMethod.GET)
-    public ModelAndView viewNumberPage(ModelAndView mav, @PathVariable String numberPageParam) {
-
-        int numberPage = Integer.parseInt(numberPageParam);
-        long count = blogService.getCount();
-        int pagesDivider = Integer.parseInt(env.getRequiredProperty("pagesDivider"));
-        int from = (numberPage - 1) * pagesDivider;
-        int to = numberPage * pagesDivider - 1;
-
-        posts = blogService.getRange(from, to);
-
-        mav.addObject("posts", posts);
-        mav.addObject("pagesCount", count / pagesDivider + (count % pagesDivider > 0 ? 1 : 0));
-        mav.setViewName("home.jsp");
-        return mav;
-    }
 
     // добавить запись
-    @RequestMapping(path = "/post/", method = RequestMethod.POST)
+    @RequestMapping(method = RequestMethod.POST)
     public Long addPost(@RequestParam(value = "name1", required = false) String name1,
                           @RequestParam(value = "text1", required = false) String text1) {
-
         long res = blogService.addPost(new Post(name1, text1));
         return res;
     }
 
     // удалить запись
-    @RequestMapping(path = "/post/{id}", method = RequestMethod.DELETE)
+    @RequestMapping(path = "/{id}", method = RequestMethod.DELETE)
     public String deletePost(@PathVariable int id) {
 
         blogService.deletePost(id);
-
         String res = "Post # " + id + " deleted";
         return res;
     }
 
     // изменить запись
-    @RequestMapping(path = "/post/{id}", method = RequestMethod.POST) // !
+    @RequestMapping(path = "/{id}", method = RequestMethod.PUT) // !
     public String updatePost(@PathVariable long id,
-                             @RequestParam(value = "name1", required = false) String name1,
-                             @RequestParam(value = "text1", required = false) String text1) {
-
-        blogService.updatePost(new Post(id, name1, text1));
-
-        return "postChanged";
+                          @RequestParam(value = "name1", required = false) String name1,
+                          @RequestParam(value = "text1", required = false) String text1){
+        blogService.updatePost(new Post(id,name1,text1));
+        String res = "Post # " + id + " was changed.";
+        return res;
     }
 
 }
